5e7e122a08499bd51a6eb85003645acd644ef6ee
brunalamberti <brunalamberti96@gmail.com>
2023-05-07 21:43:25 -0300

    Chequeo rutas

--- js/getData.js
+++ js/getData.js
@@ -1,5 +1,5 @@
 const getProducts = async () => {
-  const response = await fetch('../data/stock.json')
+  const response = await fetch("./data/stock.json")
   const data = await response.json()
 
   return data
